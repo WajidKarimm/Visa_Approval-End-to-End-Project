2064f8f970a498fddd5df5d1e7dfc8bb16cdf1fa
WajidKarimm <karimwajid0101@gmail.com>
2025-11-17 18:55:01 +0500

    created- constant.py files

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,13 @@
+import os
+from datetime import date
+
+DATABASE_NAME = "US_VISA"
+
+COLLECTION_NAME = "visa_data"
+
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPEIINE_NAME = "usvisa"
+ARTIFACT_DIR = "artifact"
+
+MODEL_FILE_NAME = "model.pkl" 
